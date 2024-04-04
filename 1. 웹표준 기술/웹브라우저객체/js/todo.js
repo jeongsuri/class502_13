118d8c22277c2610117ccbfb5683c4cdc4aaa984
jeongsuri <jeongsuri96@gmail.com>
2024-04-04 17:50:15 +0900

    no message

---
+++ 1. 웹표준 기술/웹브라우저객체/js/todo.js
@@ -0,0 +1,37 @@
+const todo = {
+    /**
+     * 스케줄 추가
+     *
+     */
+    add() {
+      const subject = frmRegist.subject.value;
+      if(!subject.trim()){ //공백제거
+        alert("할일을 입력하세요.");
+        return; //subject에 아무값도 없으면 return을 통해 더이상 진행을 하지않게한다.
+      }
+      const liEl = document.createElement("li");
+      liEl.appendChild(document.createTextNode(subject));
+  
+      const buttonEl = document.createElement("button");
+      buttonEl.appendChild(document.createTextNode("삭제"));
+      liEl.appendChild(buttonEl);
+  
+      const itemsEl = document.querySelector(".items");
+      itemsEl.appendChild(liEl);
+  
+      // 스케줄 삭제
+      buttonEl.addEventListener("click", function () {
+        itemsEl.removeChild(liEl);
+      });
+  
+      frmRegist.subject.value = "";
+      frmRegist.subject.focus();
+    },
+  };
+  
+  window.addEventListener("DOMContentLoaded", function () {
+    frmRegist.addEventListener("submit", function (e) {
+      e.preventDefault();
+      todo.add(); // 스케줄 추가
+    });
+  });
