f021efddc22a68a3f0914a1837815f20476aabc6
jeongsuri <jeongsuri96@gmail.com>
2024-04-08 16:33:05 +0900

    React 컴포넌트 스타일링 sass

---
+++ 2. 실무 프로젝트(P-1)/240408/src/components/CSSModule.js
@@ -0,0 +1,20 @@
+import styles from './CSSModule.module.scss';
+import { useState } from 'react';
+import classNames from 'classnames';
+
+console.log(styles);
+const CSSModule = () => {
+    const [visible, setVisible] = useState(false);
+    return (
+        <>
+        <div className={styles.wrapper}>
+            <span className="commonColor">안녕하세요</span><span className={styles.highlight}>반갑습니다.</span>
+        </div>
+        <div className={classNames({on: visible})}>메뉴</div>
+        
+        <button type="button" onClick={()=> setVisible(!visible)}>클릭</button>
+        </>
+
+    );
+};
+export default CSSModule;
