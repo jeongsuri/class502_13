9ea17c7a7de8b03b65770b39bf359ff6432ca25c
jeongsuri <jeongsuri96@gmail.com>
2024-04-12 11:19:56 +0900

    JAVA 클래스

---
+++ 3. 응용 SW 기초 기술활용/source/240412/src/exam01/Hello2.java
@@ -0,0 +1,11 @@
+package exam01;
+
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        for(int i=0;i<=10;i++){
+            System.out.println("*");
+
+        }
+    }
+}
---
+++ 3. 응용 SW 기초 기술활용/source/240412/src/exam01/sub/Hello.java
@@ -0,0 +1,9 @@
+package exam01.sub;
+import exam01.Hello2;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Hello2.main(null);
+    }
+}
