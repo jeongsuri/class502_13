acb80974fdeb757f1fb8d286af73753b29a2999d
jeongsuri <jeongsuri96@gmail.com>
2024-04-12 13:50:12 +0900

    자바 기본자료형 정수,실수,문자,논리

---
+++ 3. 응용 SW 기초 기술활용/source/240412_2/src/ex01/Ex03.java
@@ -0,0 +1,14 @@
+package x01;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        System.out.println(ch1); //>A
+        System.out.println(ch1 + 1); //>66
+        System.out.println();
+
+        char ch2 = '가';
+        System.out.println(ch2); //>가
+        System.out.println(ch2+1);//>44033
+    }
+}
---
+++ 3. 응용 SW 기초 기술활용/source/240412_2/src/ex01/Ex04.java
@@ -0,0 +1,6 @@
+package ex01;
+
+public class Ex04 {
+    char ch1 = 65;
+    //System.out.println(1);
+}
