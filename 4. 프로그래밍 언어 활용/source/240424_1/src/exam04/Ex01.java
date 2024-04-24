5a355bf5682041a8353ed81416e888c07f0ea03e
jeongsuri <jeongsuri96@gmail.com>
2024-04-24 17:02:22 +0900

    자바 유용한 클래스

---
+++ 4. 프로그래밍 언어 활용/source/240424_1/src/exam04/Ex01.java
@@ -0,0 +1,15 @@
+package exam04;
+
+import static java.lang.Math.*;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        System.out.println(abs(-10));
+        System.out.println(ceil(10.2));
+        System.out.println(floor(10.2));
+        System.out.println(Math.round(10.5));
+        System.out.println(Math.pow(5,2));
+        System.out.println(Math.random());
+        System.out.println(sqrt(25));
+    }
+}
