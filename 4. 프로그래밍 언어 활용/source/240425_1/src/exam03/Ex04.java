802d1d140f67ca63131518c4ab89db6cf08de90c
jeongsuri <jeongsuri96@gmail.com>
2024-04-25 17:00:28 +0900

    자바 String 메서드

---
+++ 4. 프로그래밍 언어 활용/source/240425_1/src/exam03/Ex04.java
@@ -0,0 +1,11 @@
+package exam03;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        String fruits = "Apple, Orangem Mango, Melon, Apple";
+        System.out.printf("indexOf Apple : %d%n ",fruits.indexOf("Apple"));
+        System.out.printf("lastIndexOf Apple : %d%n", fruits.lastIndexOf("Apple"));
+
+        System.out.printf("indeoxOf Banana : %d%n",fruits.indexOf("Banana"));
+    }
+}
