4166b03b612101f41c1000f0ba148d866576b0b1
jeongsuri96@gmail.com <jeongsuri96@gmail.com>
2024-04-29 09:26:05 +0900

    자바 제네릭

--- 4. 프로그래밍 언어 활용/source/240429/src/exam01/Ex01.java
+++ 4. 프로그래밍 언어 활용/source/240429/src/exam01/Ex01.java
@@ -1,4 +1,11 @@
 package exam01;
 
 public class Ex01 {
+    public static void main(String[] args) {
+        Box<Apple> appleBox = new Box<Apple>();
+        appleBox.setItem(new Apple());
+        //appleBox.setItem(new Grape()); //타입안전성 확보
+        Apple apple = appleBox.getItem(); //형변환X
+        System.out.println(apple.get());
+    }
 }
