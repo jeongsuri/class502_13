86af823aa5c7c4a7e5519a913cfd0c24fe499ed5
jeongsuri96@gmail.com <jeongsuri96@gmail.com>
2024-04-30 16:59:02 +0900

    자바 컬렉션 프레임워크

---
+++ 4. 프로그래밍 언어 활용/source/240430/src/test01/CardCompany.java
@@ -0,0 +1,14 @@
+package test01;
+
+public class CardCompany {
+    private static int serialNum = 1000;
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
