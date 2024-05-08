b96ee2490bfc3ee51b5ea6629e66621c58bce007
jeongsuri96@gmail.com <jeongsuri96@gmail.com>
2024-05-08 16:01:23 +0900

    Java time

--- 4. 프로그래밍 언어 활용/source/240508/src/exam02/Ex02.java
+++ 4. 프로그래밍 언어 활용/source/240508/src/exam02/Ex02.java
@@ -7,10 +7,17 @@ public class Ex02 {
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
 
+        /*
         int year = today.get(ChronoField.YEAR);
         int month = today.get(ChronoField.MONTH_OF_YEAR);
         int day = today.get(ChronoField.DAY_OF_MONTH);
         int yoil = today.get(ChronoField.DAY_OF_WEEK);//요일 1(월)~7(일)
+         */
+
+        int year = today.getYear();
+        int month = today.getMonthValue();
+        int day = today.getDayOfMonth();
+        int yoil = today.getDayOfWeek().getValue();
 
         System.out.printf("%d, %d, %d, %d",year,month,day,yoil);
 
