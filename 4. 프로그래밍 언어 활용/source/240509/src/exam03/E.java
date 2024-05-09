b5afa9c4320cad107149e9ae7f6abd851ee44ef7
jeongsuri96@gmail.com <jeongsuri96@gmail.com>
2024-05-09 17:00:48 +0900

    Java 형식화 클래스

--- 4. 프로그래밍 언어 활용/source/240509/src/exam03/E.java
+++ 4. 프로그래밍 언어 활용/source/240509/src/exam03/E.java
@@ -11,5 +11,8 @@ public class E {
         String num1Str = df.format(num1);
         System.out.println(num1Str); //>1,000,000,000.1230
 
+        DecimalFormat df2 = new DecimalFormat("#,###.####");
+        num1Str = df2.format(num1);
+        System.out.println(num1Str);
     }
 }
