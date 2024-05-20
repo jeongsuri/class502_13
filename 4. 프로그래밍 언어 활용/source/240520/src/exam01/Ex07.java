20dcf7c44abe1cc1a18293ce2cb977b3d47deb6c
jeongsuri96@gmail.com <jeongsuri96@gmail.com>
2024-05-20 17:10:29 +0900

    자바 쓰레드

---
+++ 4. 프로그래밍 언어 활용/source/240520/src/exam01/Ex07.java
@@ -0,0 +1,28 @@
+package exam01;
+
+public class Ex07 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread th = new Thread(() -> {
+            Thread th2 = Thread.currentThread();
+            while(true){
+                try{
+                    System.out.println("isInterrupted : " + th2.isInterrupted());
+                    for(int j = 0 ; j < 100000000L; j++);
+                    Thread.sleep(10000); //WAITING - 일시 정지
+                    //interrupted() -> InterruptedException 발생
+                    // isInterrupted - false
+                }catch(InterruptedException e){ // RUNNABLE - 실행대기
+                    System.out.println("InterruptedException 발생");
+                    th2.interrupt(); // InterruptedException,
+                }
+            }
+        });
+
+        th.start();
+
+        Thread.sleep(3000);
+        th.interrupt(); //InterruptedException 발생, isInterrupted -> true
+
+
+    }
+}
