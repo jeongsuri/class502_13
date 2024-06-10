bba2334a1a58e3f7532dcb4685554571f02efd5c
jeongsuri <jeongsuri96@gmail.com>
2024-06-10 10:15:43 +0900

    Mockito 실습

--- 9. 웹 서버 프로그램 구현/240608/src/test/java/test/JoinServiceTest.java
+++ 9. 웹 서버 프로그램 구현/240608/src/test/java/test/JoinServiceTest.java
@@ -3,6 +3,8 @@ package test;
 import global.exceptions.ValidationException;
 import member.controllers.RequestJoin;
 import member.services.JoinService;
+import member.validators.JoinValidator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,13 +14,28 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @DisplayName("회원가입기능 테스트")
 public class JoinServiceTest {
 
+    private JoinService joinService;
+
+    @BeforeEach
+    void init(){
+        joinService = new JoinService(new JoinValidator());
+    }
+
+    RequestJoin getData(){
+        return RequestJoin.builder()
+                .email("test" + System.currentTimeMillis() + "@test.org")
+                .password("12345678")
+                .confirmPassword("12345678")
+                .userName("사용자")
+                .termsAgree(true)
+                .build();
+    }
+
     @Test
     @DisplayName("회원가입 성공시 예외 발생 없음")
     void successTest(){
         assertDoesNotThrow(() -> {
-            JoinService service = new JoinService();
-            RequestJoin form = RequestJoin.builder().build();
-            service.process(form);
+            joinService.process(getData());
         });
 
     }
@@ -28,10 +45,20 @@ public class JoinServiceTest {
     void requiredFieldTest(){
 //        이메일 필수 검증 S
         assertThrows(ValidationException.class,() -> {
-            RequestJoin form = RequestJoin.builder().build();
-            JoinService service = new JoinService();
-            service.process(form);
+            RequestJoin form = getData();
+            form.setEmail(null);
+            joinService.process(form);
+            form.setEmail("     ");
+            joinService.process(form);
         });
 //        이메일 필수 검증 E
+//        비밀번호 필수 검증 S
+        assertThrows(ValidationException.class,() -> {
+            RequestJoin form = getData();
+            form.setPassword(null);
+            joinService.process(form);
+            form.setPassword("     ");
+            joinService.process(form);
+//        비밀번호 필수 검증 E
     }
 }
---
+++ 9. 웹 서버 프로그램 구현/240610/src/test/java/member/services/LoginService.java
@@ -0,0 +1,14 @@
+package member.services;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class LoginService {
+
+    public void process(HttpServletRequest request) {
+        // 아이디 : email, 비밀번호 password
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String test  =request.getParameter("123");
+        System.out.printf("email=%s, password=%s%n", email, password);
+    }
+}
