20d1bcfa998cda070ad6ae4aaac6c9fde569feb4
jeongsuri <jeongsuri96@gmail.com>
2024-07-15 09:57:15 +0900

    Spring 검증

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/validators/JoinValidator.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/validators/JoinValidator.java
@@ -4,48 +4,40 @@ package org.choongang.member.validators;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.exceptions.BadRequestException;
 import org.choongang.global.validaotrs.RequiredValidator;
-import org.choongang.global.validaotrs.Validator;
 import org.choongang.member.controllers.RequestJoin;
 import org.choongang.member.mappers.MemberMapper;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
 
 @Component
 @RequiredArgsConstructor
-public class JoinValidator implements Validator<RequestJoin>, RequiredValidator {
+public class JoinValidator implements Validator {
 
-    private final MemberMapper mapper;
 
     @Override
-    public void check(RequestJoin form) {
-        /**
-         * 1. 필수 항목 검증(email, password, confirmPassword, userName, agree)
-         * 2. 이메일 중복 여부 검증(회원이 가입되어 있는지 체크)
-         * 3. 비밀번호 자리수 체크 ( 8자리 이상 )
-         * 4. 비밀번호 + 비밀번호확인 일치 여부
-         */
+    public boolean supports(Class<?> clazz) { //RequestJoin 커맨드 객체만 검증하도록 제한
+        return clazz.isAssignableFrom(RequestJoin.class);
+    }
 
+    @Override
+    public void validate(Object target, Errors errors) {
+        // 1. 필수 항목 검증(email, password, confirmPassword, userName, agree)
+        //2. 이메일 중복 여부 검증(회원이 가입되어 있는지 체크)
+        //3. 비밀번호 자리수 체크 ( 8자리 이상 )
+        //4. 비밀번호 + 비밀번호확인 일치 여부
+
+        RequestJoin form = (RequestJoin) target;
         String email = form.getEmail();
         String password = form.getPassword();
         String confirmPassword = form.getConfirmPassword();
-        String userName = form.getUserName();
+        String userNAme = form.getUserName();
         boolean agree = form.isAgree();
 
-        // 1. 필수 항목 검증(email, password, confirmPassword, userName, agree)
-        checkRequired(email,new BadRequestException("이메일을 입력하세요."));
-        checkRequired(password,new BadRequestException("비밀번호를 입력하세요."));
-        checkRequired(confirmPassword,new BadRequestException("비밀번호를 확인하세요."));
-        checkRequired(userName,new BadRequestException("회원명을 입력하세요."));
-        checkTrue(agree,new BadRequestException("약관에 동의하세요."));
-
-        //2. 이메일 중복 여부 검증(회원이 가입되어 있는지 체크)
-        checkTrue(mapper.exists(email) == 0L , new BadRequestException("이미 가입된 이메일 입니다."));
-
-        //3. 비밀번호 자리수 체크 ( 8자리 이상 )
-        checkTrue(password.length() >= 8, new BadRequestException("비밀번호는 8자리 이상 입력하세요"));
-
-        //4. 비밀번호 + 비밀번호확인 일치 여부
-        checkTrue(password.equals(confirmPassword), new BadRequestException("비밀번호가 일치하지 않습니다."));
-
-
+        // 필수 항목 검증
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"email","Required", "이메일을 입력하세요");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"password","Required", "비밀번호를 입력하세요");
     }
 }
--- 11. 프레임워크 프로그래밍/day05/src/main/webapp/WEB-INF/templates/member/join.jsp
+++ 11. 프레임워크 프로그래밍/day05/src/main/webapp/WEB-INF/templates/member/join.jsp
@@ -3,14 +3,14 @@
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <c:url var="actionUrl" value="/member/join" />
-
 <form:form method="POST" action="${actionUrl}" autocomplete="off" modelAttribute="requestJoin">
     <dl>
         <dt>
             <spring:message code="이메일" />
         </dt>
         <dd>
-            <form:input path="email" cssClass="input-txt" cssStyle="border-color: red;"/>
+            <form:input path="email"/>
+            <form:errors path="email" element="div" delimiter=""/>
         </dd>
     </dl>
     <dl>
@@ -19,6 +19,7 @@
         </dt>
         <dd>
             <form:password path="password" />
+            <form:errors path="password"/>
         </dd>
     </dl>
     <dl>
@@ -37,36 +38,7 @@
             <form:input path="userName" />
         </dd>
     </dl>
-    <%--
-    <dl>
-        <dt>취미</dt>
-        <dd>
-            <form:radiobuttons path="hobby" items="${hobbies2}" itemLabel="code" itemValue="value" />
-               <form:select path="hobby">
-                   <option value=''>- 선택하세요 -</option>
-                   <form:option value="hobby0" label="취미0" />
-                   <form:options items="${hobbies2}" itemLabel="code" itemValue="value" />
-               </form:select>
 
-
-               <form:select path="hobby">
-                   <option value=''>- 선택하세요 -</option>
-                   <form:options items="${hobbies}" />
-               </form:select>
-
-               <form:select path="hobby" items="${hobbies}" />
-               <form:checkboxes path="hobby" items="${hobbies}" />
-        </dd>
-    </dl>
-    <dl>
-        <dt>주소</dt>
-        <dd>
-            <form:input path="addr.zipCode" placeholder="우편번호" />
-            <form:input path="addr.address" placeholder="주소" />
-            <form:input path="addr.addressSub" placeholder="나머지 주소" />
-        </dd>
-    </dl>
-    --%>
     <dl>
         <dt>
             <spring:message code="약관동의" />
