1c7ee22f8e7a660fd477edc2c06275c2c81c6170
jeongsuri <jeongsuri96@gmail.com>
2024-09-05 11:36:48 +0900

    Todo List

---
+++ 16. 리액트 네이티브/exam02/src/app/commons/components/MessageBox.js
@@ -0,0 +1,8 @@
+export default function MessageBox({children}) {
+  const messages = Array.isArray(children) ? children : [children];
+  return <>
+    {messages &&
+    messages.length > 0 &&
+    messages.map((m) => <div key={m}>{m}</div>)}
+    </>
+}
--- 16. 리액트 네이티브/exam02/src/app/todo/components/TodoList.js
+++ 16. 리액트 네이티브/exam02/src/app/todo/components/TodoList.js
@@ -5,16 +5,20 @@ import { CiSquareRemove } from 'react-icons/ci';
 const TodoList = ({ items, onToggle, onRemove }) => {
   return (
     <ul>
-      {items && items.length > 0 && items.map(({ id, title, done }) =>
-        <li key={id} onClick={() => onToggle(id)}>
-          {done ? <FaCheckSquare /> : <FaRegCheckSquare />}
-          {title}
-          <CiSquareRemove onClick={() => onRemove(id)} />
-        </li>,
-      )}
+      {items &&
+        items.length > 0 &&
+        items.map(({ id, title, content, done }) => (
+          <li key={id} onClick={() => onToggle(id)}>
+            <div>
+              {done ? <FaCheckSquare /> : <FaRegCheckSquare />}
+              {title}
+              <CiSquareRemove onClick={() => onRemove(id)} />
+            </div>
+            {content && <div>{content}</div>}
+          </li>
+        ))}
     </ul>
   );
-
 };
 
 export default TodoList;
